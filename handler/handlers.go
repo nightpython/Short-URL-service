fc63dfe2564c75a8b43dac18a3b523a4085b8949
Nikita <lawricg@gmail.com>
2021-08-22 20:30:03 +0300

    delete UserID

--- handler/handlers.go
+++ handler/handlers.go
@@ -6,29 +6,30 @@ import (
 	"github.com/my/repo/store"
 	"net/http"
 )
+
 //модель запроса
 type UrlCreationRequest struct {
-	LongUrl	string	`json:"long_url"`
-	UserId string  `json:"user_id"`
+	LongUrl string `json:"long_url"`
+	UserId  string `json:"user_id"`
 }
+
 //парсим JSON, генерируем сокращенный URL, сохраняем сопоставление короткого и длинного URL
-func CreateShortUrl(c *gin.Context)  {
+func CreateShortUrl(c *gin.Context) {
 	var creationRequest UrlCreationRequest
-	if err:=c.ShouldBindJSON(&creationRequest); err != nil{
-		c.JSON(http.StatusBadRequest, gin.H{"error":err.Error()})
+	if err := c.ShouldBindJSON(&creationRequest); err != nil {
+		c.JSON(http.StatusBadRequest, gin.H{"error": err.Error()})
 		return
 	}
-	shortUrl :=shortener.UrlGenerator(creationRequest.LongUrl, creationRequest.UserId)
-	store.SaveUrlMapping(shortUrl,creationRequest.LongUrl,creationRequest.UserId)
-	host:="http://localhost:9808/"
+	shortUrl := shortener.UrlGenerator(creationRequest.LongUrl, creationRequest.UserId)
+	store.SaveUrlMapping(shortUrl, creationRequest.LongUrl)
+	host := "http://localhost:9808/"
 	c.JSON(200, gin.H{
-		"message" : "Short url created, congratulations!",
+		"message":   "Short url created, congratulations!",
 		"short url": host + shortUrl,
 	})
 }
 
-
-func RedirectLongUrl(c *gin.Context)  {
+func RedirectLongUrl(c *gin.Context) {
 	shortUrl := c.Param("shortUrl")
 	longUrl := store.GetInitialUrl(shortUrl)
 	c.Redirect(302, longUrl)
